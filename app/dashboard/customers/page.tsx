82d1aab9ad4ed3d2a590ac9d29dba127464d50df
deedeemidlane <vietanhbg1508@gmail.com>
2024-11-06 14:50:02 +0700

    End chapter 5

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1>This is customers page</h1>
+  )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,5 @@
+export default function Page() {
+  return (
+    <h1>This is invoices page</h1>
+  )
+}
